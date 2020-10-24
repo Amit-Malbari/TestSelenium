002a8620c1ccf86163ea320d6e3fe918a3001129
amalbari <amalbari@USHYDAMALBARI1.us.gmail.com>
2020-10-25 01:19:06 +0530

    Pushing to new branch

--- FrameworkArtifact/src/test/java/loinFactory/LoginFactory.java
+++ FrameworkArtifact/src/test/java/loinFactory/LoginFactory.java
@@ -29,6 +29,7 @@ public class LoginFactory extends GeneralFunctionality{
 		System.out.println("Test instead of a");
 		System.out.println("New Line added");
 		System.out.println("New Branch");
+		System.out.println("New Branch is created and pusihing in new branch");
 		String proj_path=System.getProperty("user.dir");
 		System.out.println("b");
 		String [][]a=readExcel("C:\\Users\\amalbari\\Desktop\\Username.xlsx");
