ed06a8978b546a3c614b01ed5c2e6879945427be
Amit-Malbari <44862701+Amit-Malbari@users.noreply.github.com>
2020-10-25 01:21:11 +0530

    Update LoginFactory.java

--- FrameworkArtifact/src/test/java/loinFactory/LoginFactory.java
+++ FrameworkArtifact/src/test/java/loinFactory/LoginFactory.java
@@ -30,6 +30,7 @@ public class LoginFactory extends GeneralFunctionality{
 		System.out.println("New Line added");
 		System.out.println("New Branch");
 		System.out.println("New Branch is created and pusihing in new branch");
+		System.out.println("New Branch is created and pusihing in new branch1");
 		String proj_path=System.getProperty("user.dir");
 		System.out.println("b");
 		String [][]a=readExcel("C:\\Users\\amalbari\\Desktop\\Username.xlsx");
